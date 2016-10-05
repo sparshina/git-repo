caf81af767f8cd63dd2be7f6f1a069d6c4fec924
Sonya Parshina <ssp_lr_9997@mail.ru>
2016-10-06 01:40:52 +0500

    Задача 3

---
+++ Сакод ЛБ1/задача 3/Задача 3.cs
@@ -0,0 +1,28 @@
+﻿//3.	Дано натуральное число. Найти число, получаемое в результате приписывания по двойке в начало и конец записи исходного числа.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задача_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите исходное число:");
+            int a = int.Parse(Console.ReadLine());
+            int count = 0; //число цифр исходного числа
+            double b = a;
+            while (a > 0)
+            {
+                a /= 10;
+                count++;
+            }
+            b += 2 * Math.Pow((double)10,(double)count);
+            b = b * 10 + 2;
+            Console.WriteLine(b);
+        }
+    }
+}
